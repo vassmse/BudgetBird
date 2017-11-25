8d0fdee0002e739a964682eba42878fa43535752
Emese Vass <vassmse@gmail.com>
2017-11-25 17:50:08 +0100

    history: datepicker added

--- app/src/main/java/hu/bme/aut/budgetbird/activities/History.java
+++ app/src/main/java/hu/bme/aut/budgetbird/activities/History.java
@@ -1,15 +1,25 @@
 package hu.bme.aut.budgetbird.activities;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 import hu.bme.aut.budgetbird.R;
 
 public class History extends AppCompatActivity {
 
+    private DatePicker datePicker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        datePicker = (DatePicker) findViewById(R.id.hisorty_datepicker);
+
     }
 }
